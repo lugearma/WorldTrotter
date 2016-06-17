38c15ff5e906a9aebd526a241e717fbc6b63fce9
Luis Arias <lugearma@gmail.com>
2016-06-17 13:36:46 -0500

    Added view programmatically

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let firstFrame = CGRect(x: 120, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.orangeColor()
+        self.view.addSubview(firstView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
