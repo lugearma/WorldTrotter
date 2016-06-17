60dde585783ee1e6c989539a5012cf08686b9c67
Luis Arias <lugearma@gmail.com>
2016-06-17 14:15:43 -0500

    Added views

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -12,16 +12,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let firstFrame = CGRect(x: 120, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.orangeColor()
-        self.view.addSubview(firstView)
-        
-        let secondFrame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.brownColor()
-        self.view.addSubview(secondView)
     }
 
 }
