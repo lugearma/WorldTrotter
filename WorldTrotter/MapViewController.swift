5989fa23bff7fb273a25ddf53e0e45991f1c9fbf
Luis Arias <lugearma@gmail.com>
2016-07-01 13:10:49 -0500

    Added anchors

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -16,6 +16,20 @@ class MapViewController: UIViewController {
     override func loadView() {
         self.mapView = MKMapView()
         self.view = mapView
+        
+        let segmentControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        segmentControl.selectedSegmentIndex = 0
+        segmentControl.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(segmentControl)
+        
+        let topConstrain = segmentControl.topAnchor.constraintEqualToAnchor(self.view.topAnchor)
+        let leadingContrain = segmentControl.leadingAnchor.constraintEqualToAnchor(self.view.leadingAnchor)
+        let trailingContrain = segmentControl.trailingAnchor.constraintEqualToAnchor(self.view.trailingAnchor)
+        
+        topConstrain.active = true
+        leadingContrain.active = true
+        trailingContrain.active = true
     }
     
     override func viewDidLoad() {
